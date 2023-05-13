492547f383d6c91ce6a063ae611a2b152a726c03
Obadara16 <obadararoqeeb@gmail.com>
2023-05-13 06:41:58 +0100

    feat: added .d.ts file

--- client/src/components/CardDesign.jsx
+++ client/src/components/CardDesign.jsx
@@ -1,46 +1,46 @@
-import React, { useContext, useState } from 'react'
-import PaymentCard from 'react-payment-card-component'
-import { AppContext } from '../context/usercontext'
+import React, { useContext, useState } from "react";
+import ReactPaymentCard from "react-payment-card-component";
+import { AppContext } from "../context/usercontext";
 
 const CardDesign = () => {
-    const [isFlipped, setIsFlipped] = useState(false)
+  const [isFlipped, setIsFlipped] = useState(false);
 
   const handleFlip = () => {
-    setIsFlipped(!isFlipped)
-  }
+    setIsFlipped(!isFlipped);
+  };
 
   const user = useContext(AppContext);
-  const userInfo = user.userProfile
-  const fullName = userInfo.firstName + " " + userInfo.lastName
+  const userInfo = user.userProfile;
+  const fullName = userInfo.firstName + " " + userInfo.lastName;
 
   return (
     <div onClick={handleFlip}>
-        <PaymentCard
-      bank="nubank"
-      model="normal"
-      type="platinum"
-      brand="mastercard"
-      number="4111111111111111"
-      cvv="202"
-      holderName={fullName}
-      expiration="12/20"
-      flipped={isFlipped}
-    />
+      <ReactPaymentCard
+        bank="nubank"
+        model="normal"
+        type="platinum"
+        brand="mastercard"
+        number="4111111111111111"
+        cvv="202"
+        holderName={fullName}
+        expiration="12/20"
+        flipped={isFlipped}
+      />
     </div>
-//     <div onClick={handleFlip}>
-//     <PaymentCard
-//   bank="nubank"
-//   model="normal"
-//   background="#a72298"
-//   brand="mastercard"
-//   number="4111111111111111"
-//   cvv="202"
-//   holderName="Eleanor Pena"
-//   expiration="12/20"
-//   flipped={isFlipped}
-// />
-// </div>
-  )
-}
+    //     <div onClick={handleFlip}>
+    //     <PaymentCard
+    //   bank="nubank"
+    //   model="normal"
+    //   background="#a72298"
+    //   brand="mastercard"
+    //   number="4111111111111111"
+    //   cvv="202"
+    //   holderName="Eleanor Pena"
+    //   expiration="12/20"
+    //   flipped={isFlipped}
+    // />
+    // </div>
+  );
+};
 
-export default CardDesign
+export default CardDesign;
